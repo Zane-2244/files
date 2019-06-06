b361269e44fc01ca658946b7bedd2effa08dd13d
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-06-06 13:47:12 +0200

    Add files via upload

---
+++ FEXE1/FEXE1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+
+namespace FEXE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"...";
+            File.ReadAllText(path);
+            Console.WriteLine(File.GetAttributes(path));
+        }
+    }
+}
---
+++ FEXE2/FEXE2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+
+namespace FEXE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"...";
+            File.ReadAllText(path);
+            Directory.GetFiles(path);
+            Console.WriteLine(Path.GetExtension(path));
+
+        }
+    }
+}
